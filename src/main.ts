9382b1e1577981a127b1deae9297fe0dc3719bff
Ray Smets <rayjsmets@gmail.com>
2021-01-15 17:29:11 -0800

    [Exception Filters] advanced topics: exception inheritance and filtering at the init level.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/exception-filters/http-exception.filter';
 import { logger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); // explicitly defining use of express platform (which is the default) ref: https://docs.nestjs.com/first-steps
   // app.use(logger); // binding functional middleware globally
   // app.useGlobalFilters(new HttpExceptionFilter()); // global scoped exception filter
+
+  // Using a customer catch all exception filter with inheritance that is actually using a base filter. So just like the HttpExceptionFilter class.
+  // ref: https://docs.nestjs.com/exception-filters#inheritance
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
